6aa500ad91f136219544a64b9d0964550b483251
BoYiZhang <zhangboyi_mx@163.com>
2021-03-03 10:10:34 +0800

    add mark

--- src/main/scala/com/boyi/datasource/SocketSource.scala
+++ src/main/scala/com/boyi/datasource/SocketSource.scala
@@ -3,6 +3,21 @@ package com.boyi.datasource
 import org.apache.flink.api.scala.createTypeInformation
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 
+
+//  示例
+//    编写Flink程序，接收 socket 的单词数据，并以空格进行单词拆分打印。
+//    步骤
+//      1. 获取流处理运行环境
+//      2. 构建socket流数据源，并指定IP地址和端口号
+//      3. 对接收到的数据进行空格拆分
+//      4. 打印输出
+//      5. 启动执行
+//      6. 在Linux中，使用 nc -lk 端口号 监听端口，并发送单词
+//
+//  安装nc: yum install -y nc
+//  nc -lk 6666 监听6666端口的信息
+
+
 object SocketSource {
 
   def main (args : Array[String]) : Unit = {
