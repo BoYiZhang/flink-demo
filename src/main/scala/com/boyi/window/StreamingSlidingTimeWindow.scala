70228ec216281f0c2ce2b2f8d7885e2e5ed42edb
BoYiZhang <zhangboyi_mx@163.com>
2021-03-03 20:42:40 +0800

    Time Window Op

--- src/main/scala/com/boyi/window/StreamingSlidingTimeWindow.scala
+++ src/main/scala/com/boyi/window/StreamingSlidingTimeWindow.scala
@@ -1,14 +1,13 @@
 package com.boyi.window
 
-
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 
 
 // 要先打开 服务端,否则报错 ::     nc -lk 9999
-// 滚动时间计数器窗口
-object StreamingTumblingTimeWindow {
+// 滑动时间计数器窗口
+object StreamingSlidingTimeWindow {
   def main(args: Array[String]): Unit = {
     // 1. 创建流处理环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -17,13 +16,12 @@ object StreamingTumblingTimeWindow {
     // 3.处理数据
     val data = textStream.flatMap(x=> x.split(" ")).filter( x=>{ null!=x && x.trim.length != 0}).map(x=>{(x,1)})
 
-
     //4. 根据key做聚合操作..
-   val keyByData = data.keyBy(_._1)
+    val keyByData = data.keyBy(_._1)
 
-    //5. 执行统计操作，每5秒钟统计一下各单词的数量
-    val result = keyByData.window(TumblingProcessingTimeWindows.of(Time.seconds(5))).sum(1)
-
+    //5. 执行统计操作，步长为2, 每5秒钟统计一下各单词的数量
+    // TumblingProcessingTimeWindows
+    val result= keyByData.window(TumblingProcessingTimeWindows.of(Time.seconds(5),Time.seconds(2))).sum(1)
     //6. 显示统计结果
     result.print()
     //7. 触发流计算
