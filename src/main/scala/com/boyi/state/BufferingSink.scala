fdb5f9010a1d9b094f32abca5911fb13e5e96192
BoYiZhang <zhangboyi_mx@163.com>
2021-03-09 16:38:21 +0800

    CheckpointedFunction  Op

---
+++ src/main/scala/com/boyi/state/BufferingSink.scala
@@ -0,0 +1,54 @@
+package com.boyi.state
+
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.api.common.typeinfo.{TypeHint, TypeInformation}
+import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
+import org.apache.flink.streaming.api.functions.sink.SinkFunction.Context
+
+import scala.collection.mutable.ListBuffer
+
+class BufferingSink(threshold: Int = 0)
+  extends SinkFunction[(String, Int)]
+    with CheckpointedFunction {
+
+  @transient
+  private var checkpointedState: ListState[(String, Int)] = _
+
+  private val bufferedElements = ListBuffer[(String, Int)]()
+
+  override def invoke(value: (String, Int), context: Context): Unit = {
+    bufferedElements += value
+    if (bufferedElements.size == threshold) {
+      for (element <- bufferedElements) {
+        // send it to the sink
+      }
+      bufferedElements.clear()
+    }
+  }
+
+  override def snapshotState(context: FunctionSnapshotContext): Unit = {
+    checkpointedState.clear()
+
+    bufferedElements.foreach(element => {
+      checkpointedState.add(element)
+    })
+  }
+
+  override def initializeState(context: FunctionInitializationContext): Unit = {
+    val descriptor = new ListStateDescriptor[(String, Int)](
+      "buffered-elements",
+      TypeInformation.of(new TypeHint[(String, Int)]() {})
+    )
+
+    checkpointedState = context.getOperatorStateStore.getListState(descriptor)
+
+    if(context.isRestored) {
+      val iterate  = checkpointedState.get()
+      iterate.forEach(element => bufferedElements += element )
+
+    }
+  }
+
+}
